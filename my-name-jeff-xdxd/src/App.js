5a2cc216b7a9a17e07d9d7f3530f93c3bf94d49b
Andrew <al4991@nyu.edu>
2019-04-24 12:03:33 -0400

    Implemented square div that has value go from visible to not between clicks

--- my-name-jeff-xdxd/src/App.js
+++ my-name-jeff-xdxd/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+//
+// function App() {
+//   return (
+//     <div>
+//         ohfoashfao
+//     </div>
+//   );
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Square extends React.Component {
+    constructor() { 
+        super();
+        this.state = { 
+            isHidden: true,
+            value: '1'
+        }
+    }
+    toggleHidden() {
+        this.setState({
+            isHidden: !this.state.isHidden
+        })
+    }
+
+    render() {
+        return (
+            <div className="card" onClick={this.toggleHidden.bind(this)} >
+                {this.state.isHidden && this.state.value}
+            </div>
+        )
+    }
 }
 
-export default App;
+
+export default Square;
